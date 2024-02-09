3b3b06efb6cdba305a7fc69974b7dcbecaf1cd21
Joao Paulo <joaopauloj405@gmail.com>
2024-02-09 20:29:41 -0300

    feat: add basic paths (login+register)

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login to your account",
+};
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+}
---
+++ src/paths.ts
@@ -0,0 +1,8 @@
+const paths = {
+  home: "/",
+  login: "/login",
+  register: "/register",
+  notFound: "/not-found",
+};
+
+export default paths;
