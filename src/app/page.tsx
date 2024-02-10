30a721d9ca9045450850797ad5ca646bd6437d92
Joao Paulo <joaopauloj405@gmail.com>
2024-02-10 20:37:32 -0300

    feat: add navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-export default function Home() {
+import Navbar from "@/components/navbar";
+import { getSession, logout } from "./lib/actions";
+
+export default async function Home() {
+  const session = await getSession();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Home</h1>
+    <main className="flex min-h-screen flex-col items-center m-0">
+      <Navbar />
+      <div className="border m-8 p-4">
+        <pre>{JSON.stringify(session, null, 2)}</pre>
       </div>
     </main>
   );
---
+++ src/components/navbar.tsx
@@ -0,0 +1,30 @@
+import Logo from "/home/jp/Documentos/Projects/mvp-audita/mvp-audita-application/public/logo.svg";
+import Image from "next/image";
+import { logout } from "@/app/lib/actions";
+import { Button } from "./ui/button";
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-6 px-8 w-full border-b">
+      <div className="flex-shrink-0">
+        <Image src={Logo} width={30} height={30} alt="logo" />
+      </div>
+      <div className="mx-auto">
+        <p className="text-xl text-muted-foreground">
+          Transparência e responsabilidade
+        </p>
+      </div>
+      <div className="flex-shrink-0">
+        <form
+          action={async () => {
+            "use server";
+            await logout();
+          }}
+        >
+          <Button type="submit" variant={"secondary"}>
+            Sair
+          </Button>
+        </form>
+      </div>
+    </nav>
+  );
+}
