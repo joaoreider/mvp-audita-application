d9fd3746bcfbdd939410bcf5aa26a2850a185534
Joao Paulo <joaopauloj405@gmail.com>
2024-02-13 19:43:19 -0300

    fix: remove Uploader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
 "use client";
 import Navbar from "@/components/navbar";
-import { getSession } from "./lib/actions";
-import { useEffect, useState } from "react";
+
 import { ClimbingBoxLoader } from "react-spinners";
-import { useRouter } from "next/navigation";
-import Uploader from "@/components/uploader";
+
 import SubmitButton from "@/components/submit-button";
 import { SessionData } from "./lib/types";
 import { v4 as uuidv4 } from "uuid";
+
+import { FaUpload } from "react-icons/fa6";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { toast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import FileItem from "@/components/file-item";
 
 const formatDate = (date: Date) => {
   const day = date.getDate().toString().padStart(2, "0");
@@ -19,45 +36,154 @@ const formatDate = (date: Date) => {
   return `${day}-${month}-${year}T${hours}:${min}`;
 };
 
+export type Status = "ok" | "pending" | "error";
+type FileUploaded = {
+  name: string;
+  size: number;
+  progress: number;
+  status: Status;
+};
+
 export default function Home() {
-  const router = useRouter();
   const UPLOAD_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL + "/files/upload";
   // código da análise: data-hora-uuid  (ex: 01/01/2022-12:00|1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed)
   const [analysisCode, setAnalysisCode] = useState<string>(
     formatDate(new Date()) + "|" + uuidv4()
   );
+  const [loading, setLoading] = useState(false);
+  const [uploadedFiles, setUploadedFiles] = useState<FileUploaded[]>([]);
+  const [canUpload, setCanUpload] = useState<boolean>(true);
 
-  const [loading, setLoading] = useState(true);
-  const [session, setSession] = useState<SessionData | null>(null);
-
-  useEffect(() => {
-    setLoading(true);
-    const fetchSession = async () => {
-      try {
-        const session = await getSession();
-        setSession(session);
-        if (!session) {
-          router.push("/login");
-        }
-      } catch (error) {
-        router.push("/login");
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchSession();
-  }, [router]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const maxFileSize = 5;
+  const MAX_FILE_BYTES = maxFileSize * 1024 * 1024; // 5MB to bytes
 
   const handleStartAnalysis = async () => {
+    if (uploadedFiles.length === 0) {
+      toast({
+        variant: "destructive",
+        description: "Nenhum arquivo carregado para análise.",
+      });
+      return;
+    }
+    if (uploadedFiles.some((file) => file.status !== "ok")) {
+      toast({
+        variant: "destructive",
+        description: "Aguarde o carregamento dos arquivos.",
+      });
+      return;
+    }
     setLoading(true);
-    await new Promise((resolve) => setTimeout(resolve, 200));
-    console.log("Analisar propostas com código: ", analysisCode);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     setAnalysisCode(formatDate(new Date()) + "|" + uuidv4());
     setLoading(false);
   };
 
+  useEffect(() => {
+    setCanUpload(uploadedFiles.length < 5);
+  }, [uploadedFiles]);
+
   const handleResetAnalysis = () => {
     setAnalysisCode(formatDate(new Date()) + "|" + uuidv4());
+    setUploadedFiles([]);
+  };
+
+  const handleDelete = (name: string) => {
+    setUploadedFiles(uploadedFiles.filter((item) => item.name !== name));
+  };
+
+  const handleUpdateFile = (
+    file: File,
+    status: Status = "pending",
+    progress: number
+  ) => {
+    handleDelete(file.name);
+    setUploadedFiles((prev) => [
+      ...prev,
+      {
+        name: file.name,
+        size: file.size,
+        progress: progress,
+        status: status,
+      },
+    ]);
+  };
+
+  const fileAlreadyUploaded = (file: File) => {
+    return uploadedFiles.some((f) => f.name === file.name);
+  };
+
+  const verifyIfCanUpload = (file: File) => {
+    if (file.size > MAX_FILE_BYTES) {
+      toast({
+        variant: "destructive",
+        description: `Tamanho máximo de arquivo permitido: ${maxFileSize} MB`,
+      });
+      return false;
+    }
+    if (fileAlreadyUploaded(file)) {
+      toast({
+        description: "Esse arquivo já foi carregado",
+      });
+      return false;
+    }
+    if (uploadedFiles.length >= 5) {
+      return false;
+    }
+    return true;
+  };
+
+  const fileSelectedHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const files = Array.from(event.target.files);
+      for (const file of files) {
+        if (verifyIfCanUpload(file)) {
+          handleUpdateFile(file, "pending", 0);
+          fileUploadHandler(file);
+        }
+      }
+    }
+  };
+
+  const fileUploadHandler = (file: File) => {
+    // clear upload reference
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+    try {
+      // Setup the form data to send
+      const formData = new FormData();
+      formData.append("files", file);
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", UPLOAD_URL, true);
+      xhr.setRequestHeader("analysis-code", analysisCode);
+
+      // Upload progress
+      xhr.upload.addEventListener("progress", (event) => {
+        if (event.lengthComputable) {
+          const progress = Math.round((event.loaded / event.total) * 100);
+          progress < 100
+            ? handleUpdateFile(file, "pending", progress)
+            : handleUpdateFile(file, "ok", progress);
+        }
+      });
+
+      // File uploaded
+      xhr.addEventListener("readystatechange", () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 201) {
+            handleUpdateFile(file, "ok", 100);
+          } else {
+            console.error("Error uploading file: ", xhr.responseText);
+            handleUpdateFile(file, "error", 0);
+          }
+        }
+      });
+      xhr.send(formData);
+    } catch (error) {
+      handleUpdateFile(file, "error", 0);
+      console.error("Error uploading file: ", error);
+    }
   };
 
   if (loading) {
@@ -71,11 +197,91 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center m-0">
         <Navbar />
         <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col items-center justify-center rounded-md border m-12 p-2  shadow-[0_20px_50px_rgba(8,_112,_184,_0.1)]">
-          <Uploader
-            url={UPLOAD_URL}
-            analysisCode={analysisCode}
-            resetAnalysis={handleResetAnalysis}
-          />
+          <div className="container">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button variant="outline" className="my-1">
+                  Nova análise
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Essa ação é definitiva e exclui todos os arquivos
+                    carregados.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction onClick={handleResetAnalysis}>
+                    Continue
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+
+            <div className="wrapper">
+              <form
+                action="#"
+                className="relative flex flex-col items-center justify-center  p-2 w-full"
+              >
+                <label
+                  htmlFor="dropzone-file"
+                  className="flex flex-col items-center justify-center w-full h-64 cursor-pointer"
+                >
+                  <div className="flex flex-col items-center text-center justify-center pt-5 pb-6">
+                    <FaUpload size={42} color="white" className="mb-4" />
+                    {canUpload ? (
+                      <span className="leading-7  text-muted-foreground">
+                        Faça o upload de até{" "}
+                        {5 - uploadedFiles.length === 1
+                          ? `1 arquivo `
+                          : `${5 - uploadedFiles.length} arquivos `}
+                        (5MB cada)
+                      </span>
+                    ) : (
+                      <span className="leading-7  text-muted-foreground text-yellow-400">
+                        Limite de arquivos atingido
+                      </span>
+                    )}
+                  </div>
+
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    onChange={fileSelectedHandler}
+                    multiple={false}
+                    className="hidden"
+                    ref={fileInputRef}
+                    disabled={!canUpload}
+                    accept=".xls,.xlsx,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                  />
+                </label>
+              </form>
+            </div>
+
+            <ScrollArea className="w-full px-10">
+              <div className="flex flex-col overflow-y-auto max-h-[400px] p-2 w-full items-center justify-center">
+                {uploadedFiles.map((file, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="flex flex-col w-full p-0 m-0 items-center justify-center"
+                    >
+                      <FileItem
+                        name={file.name}
+                        size={file.size}
+                        progress={file.progress}
+                        status={file.status}
+                        onDelete={() => handleDelete(file.name)}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </ScrollArea>
+          </div>
         </div>
         <div className="flex flex-col">
           <SubmitButton
@@ -87,6 +293,4 @@ export default function Home() {
       </main>
     );
   }
-}
-{
 }
